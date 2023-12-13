e5456644e13b575cf864eb74c52b5be783877461
TechiAkki963 <paraba777@gmail.com>
2023-12-13 17:35:40 +0530

    Console.log

---
+++ 01-Variable-Datatype/script.js
@@ -0,0 +1,19 @@
+//**log a string ""
+console.log("Hello World!!!");
+
+//**log a number */
+console.log(100);
+
+//**log multiple values at a time */
+console.log(2070, "Doremon", true);
+
+//**log a Variable  */
+const x = 30;
+console.log(x);
+
+//**log multiple Variable at a time */
+const y = 80;
+console.log(x, y);
+
+//**log in a tabular format */
+console.table({ name: "Shizuka", email: "Shizuka@xyx.com" });
