cacc384591a08cd89d3bda5f7b7e653a8d4d7854
TechiAkki963 <paraba777@gmail.com>
2023-12-15 21:04:40 +0530

    Naming convention Variable

---
+++ 01_Variable_Datatypes/script.js
@@ -0,0 +1,27 @@
+// Way to declare Variable
+// var , let , const
+
+let firstName = "John";
+
+let lastName = "Doe";
+
+console.log(firstName, lastName);
+
+/*
+console.log(age);
+//! Cannot access 'age' before initialization
+var age = 30;
+*/
+
+let age = 30;
+console.log(age);
+
+//Naming Conventions
+//-Only letters, numbers, underscores and dollarsigns
+//-Can't start with a number
+
+//Multiple Formatting
+//* firstName = camelCase
+//* FirstName = PascalCase
+// first_name = underscore
+// firstname = lowercase
