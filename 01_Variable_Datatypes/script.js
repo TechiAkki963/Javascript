def31a00811b8832441d2d82aa375ccd46a8c855
TechiAkki963 <paraba777@gmail.com>
2023-12-15 21:17:21 +0530

    variable reassign

--- 01_Variable_Datatypes/script.js
+++ 01_Variable_Datatypes/script.js
@@ -25,3 +25,32 @@ console.log(age);
 //* FirstName = PascalCase
 // first_name = underscore
 // firstname = lowercase
+
+//************************************* */
+// todo : Reassign Variable
+//* using "let"
+
+age = 31;
+console.log(age);
+
+let score;
+console.log(score);
+
+score = 100;
+console.log(score);
+
+// todo : Reassign Variable
+//* using "const"
+const x = 50;
+console.log(x);
+/*
+x = 51;
+console.log(x); 
+//! Error : Assignment to constant variable
+*/
+
+/*
+const y;
+console.log(y);
+//! SyntaxError: Missing initializer in const declaration
+*/
