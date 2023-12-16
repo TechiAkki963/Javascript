402fa7b770a8e4b00506698ccc2cf59a50c597cd
TechiAkki963 <paraba777@gmail.com>
2023-12-16 21:24:16 +0530

    Datatypes

---
+++ 02_Datatypes/script.js
@@ -0,0 +1,13 @@
+//?Primitive Datatype
+// String - Sequence of Character. Must be in " " or ` `.
+// Number - Integers as well as floating-point numbers.
+// Boolean - Logical entity / True or False.
+// Null - Intentionally absence of any object value.
+// Undefined - A variable that has not yet been assigned / defined.
+// Symbol - built-in object whose constructor return a unique symbol.
+// BigInt - Numbers that are greater than the "Number" type can handle.
+
+//? Reference type
+//Reference type or "Object" are a non-primitive valueand when assigned to a variable, the variable is given a reference to that value.
+
+//Object literals, Arrays and Functions are all reference type
