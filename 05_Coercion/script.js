dbed6ae77ebfcf48bcd6d9b2d366571028a8ad34
akshay <paraba777@gmail.com>
2023-12-28 23:12:05 +0530

    Commenting Output

--- 05_Coercion/script.js
+++ 05_Coercion/script.js
@@ -1,23 +1,23 @@
 let x;
 
-x = 5 + "5"; //55 String
+x = 5 + "5"; //Output: 55 String
 
-x = 5 + Number("5"); //10 number
+x = 5 + Number("5"); //Output: 10 number
 
-x = 5 * "5"; //25 number
+x = 5 * "5"; //Output: 25 number
 
-x = 5 + undefined; //NaN number
+x = 5 + undefined; //Output: NaN number
 
-x = 5 + null; // 5 number
+x = 5 + null; //Output:  5 number
 
-x = Number(null); //0 number
+x = Number(null); //Output: 0 number
 
-x = Number(true); //1 number
+x = Number(true); //Output: 1 number
 
-x = Number(false); //0 number
+x = Number(false); //Output: 0 number
 
-x = 5 + true; //6 number
+x = 5 + true; //Output: 6 number
 
-x = 5 + false; //5  number
+x = 5 + false; //Output: 5  number
 
 console.log(x, typeof x);
