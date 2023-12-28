ecdca3ccb3ff1704e76e9a33ca9dcef9f340485a
akshay <paraba777@gmail.com>
2023-12-28 16:58:11 +0530

    Operators

---
+++ 04_Operators/script.js
@@ -0,0 +1,76 @@
+// todo:  Arithmetic Operators;
+
+let x = 5;
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
+
+console.log(x);
+
+//todo : Concatenation
+x = "hello " + "world";
+console.log(x);
+//or
+x = "hello" + " " + "world";
+console.log(x);
+
+//todo : Exponent
+x = 2 ** 3;
+console.log(x);
+
+//todo : Increment
+x = 1;
+// x = x + 1;
+x++;
+console.log(x);
+
+//todo : Decrement
+// x=x-1;
+x--;
+console.log(x);
+
+//?
+//todo : Assignment Operators
+
+x = 5;
+
+x += 5; //x = 5 + 5; = 10
+console.log(x);
+
+x -= 5; //x = 10 - 5; = 5
+console.log(x);
+
+x *= 5; //x = 5 * 5; =25
+console.log(x);
+
+x /= 5; //x= 25 / 5; =5
+console.log(x);
+
+x %= 5; //x = 5 % 5; =0
+console.log(x);
+
+x **= 5; //x = 0**5; 0
+console.log(x);
+
+//todo : Comparison Operator  == and ===
+
+x = 2 == "2";
+console.log(x); // true
+
+x = 2 === "2"; //**always use === */
+console.log(x); //false
+
+x = 2 != "2";
+console.log(x);
+
+x = 2 !== "2"; //**always use !== */
+console.log(x);
+
+//? Greater than or Less than
+x = 10 > 5;
+x = 10 < 5;
+x = 10 >= 5;
+x = 10 <= 5;
+console.log(x);
