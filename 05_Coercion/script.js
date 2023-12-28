da97c96c6655dcd47ff0c7f7a9e806811f90802b
akshay <paraba777@gmail.com>
2023-12-28 23:04:48 +0530

    Type Coersion

---
+++ 05_Coercion/script.js
@@ -0,0 +1,23 @@
+let x;
+
+x = 5 + "5"; //55 String
+
+x = 5 + Number("5"); //10 number
+
+x = 5 * "5"; //25 number
+
+x = 5 + undefined; //NaN number
+
+x = 5 + null; // 5 number
+
+x = Number(null); //0 number
+
+x = Number(true); //1 number
+
+x = Number(false); //0 number
+
+x = 5 + true; //6 number
+
+x = 5 + false; //5  number
+
+console.log(x, typeof x);
