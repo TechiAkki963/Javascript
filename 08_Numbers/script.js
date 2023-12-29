5c0d0d7b72a3a1eedf3ec2ea335d7e0e17594aa7
akshay <paraba777@gmail.com>
2023-12-29 22:54:50 +0530

    Numbers

---
+++ 08_Numbers/script.js
@@ -0,0 +1,23 @@
+let x;
+
+const num = new Number(5.4527);
+
+x = num.toString(); //
+
+x = num.toString().length; //
+
+x = num.toFixed(2); //5.45
+
+x = num.toPrecision(2); //5.5
+
+x = num.toExponential(2); //5.00e+0
+
+x = num.toLocaleString("en-US"); //5
+
+x = num.valueOf();
+
+x = Number.MAX_VALUE; // 1.7976931348623157e+308
+
+x = Number.MIN_VALUE; // 5e-324
+
+console.log(x, typeof x);
