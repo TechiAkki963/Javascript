cb0afce8e06fcc74190c3c9d7a207d9e2383faba
akshay <paraba777@gmail.com>
2023-12-29 14:58:33 +0530

    comments -line 4

--- 07-Capitalize-Challenge/script.js
+++ 07-Capitalize-Challenge/script.js
@@ -1,8 +1,8 @@
 const myString = "developer";
 
 //Solution 1
-let newString =
-  myString.charAt(0).toUpperCase() + myString.slice(1, myString.length);
+// let newString =
+//   myString.charAt(0).toUpperCase() + myString.slice(1, myString.length);
 
 //Solution 2
 let newString = `${myString.charAt(0).toUpperCase()}${myString.slice(
