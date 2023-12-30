583e73c8d58800c8ba8ad8f86f166ce5c004ef23
akshay <paraba777@gmail.com>
2023-12-30 11:45:41 +0530

    Strings

--- 06_Strings/script.js
+++ 06_Strings/script.js
@@ -11,42 +11,41 @@ const a = "Hello World";
 // const a = new String("Hello World");    how to turn a string into string object
 //x = typeof a;
 
-x = a.length;
+x = a.length; // 11
 
-x = a[1]; // acess value by key
+x = a[1]; //e
 
-x = a.charAt(0);
+x = a.charAt(0); //H
 
-x = a.__proto__;
+x = a.__proto__; //String {'', constructor: ƒ, anchor: ƒ, at: ƒ, big: ƒ, …}
 
-x = a.toUpperCase();
+x = a.toUpperCase(); //HELLO WORLD
 
-x = a.toLowerCase();
+x = a.toLowerCase(); //hello world
 
-x = a.indexOf("d");
+x = a.indexOf("d"); //10
 
-x = a.substring(2, 5);
+x = a.substring(2, 5); //llo
 
-x = a.substring(7);
+x = a.substring(7); //orld
 
-x = a.slice(2, 5);
+x = a.slice(2, 5); //llo
 
-x = a.slice(-11, 6);
+x = a.slice(-11, 6); //Hello
 
 //todo : trim() the white spaces
 x = "      hello world";
-x = a.trim(); //trim the space
+x = a.trim(); //trim the space    Hello World
 
 //todo : replace
-x = a.replace("World", "John");
+x = a.replace("World", "John"); // Hello John
 
 //todo : includes
-x = a.includes("Hell");
+x = a.includes("Hell"); //true
 
-x = a.valueOf(); // to know the actual value
+x = a.valueOf(); // to know the actual value   "Hello World"
 
-x = a.split(""); // to split characters
+x = a.split(""); // to split characters     (11) ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
 
-x = a.split(" "); // to split words
-
+x = a.split(" "); // to split words     (2) ['Hello', 'World']
 console.log(x);
