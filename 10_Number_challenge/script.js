72dca067c1347329a8917bee90f12b3c535a3874
akshay <paraba777@gmail.com>
2023-12-30 16:35:14 +0530

    Exercise Solution

---
+++ 10_Number_challenge/script.js
@@ -0,0 +1,23 @@
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+let sum = x + y;
+let sumOutput = `${x} + ${y}= ${sum}`;
+
+// Difference
+let diff = x - y;
+let diffOutput = `${x} - ${y} = ${diff}`;
+
+// Multiplication
+const mul = x * y;
+const mulOutput = `${x} * ${y} = ${mul}`;
+
+const div = x / y;
+const quotient = `${x} / ${y} = ${div}`;
+
+console.log(x);
+console.log(y);
+console.log(sumOutput);
+console.log(diffOutput);
+console.log(mulOutput);
+console.log(quotient);
