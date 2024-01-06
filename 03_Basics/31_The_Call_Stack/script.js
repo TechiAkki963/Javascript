f4ad920025f093a963af830f91c53a404cd27f1e
Akshay <paraba777@gmail.com>
2024-01-06 21:02:32 +0530

    Call Stack

---
+++ 03_Basics/31_The_Call_Stack/script.js
@@ -0,0 +1,36 @@
+function first() {
+  console.log(` The First`);
+}
+
+function second() {
+  console.log(` The Second`);
+}
+
+function third() {
+  console.log(` The Third`);
+}
+
+first();
+second();
+third();
+
+/*
+function first() {
+  console.log(` The First`);
+  second();
+}
+
+function second() {
+  console.log(` The Second`);
+  third();
+}
+
+function third() {
+  console.log(` The Third`);
+}
+
+first();
+second();
+third();
+
+*/
