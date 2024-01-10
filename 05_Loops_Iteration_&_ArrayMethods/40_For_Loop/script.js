f24351a21f88a6fe536bf0800f70b1bf11d04a47
Akshay <paraba777@gmail.com>
2024-01-11 00:05:59 +0530

    For Loop

--- 05_Loops_Iteration_&_ArrayMethods/40_For_Loop/script.js
+++ 05_Loops_Iteration_&_ArrayMethods/40_For_Loop/script.js
@@ -2,24 +2,27 @@
 
 // for ([initialization];[condition];[increment/decrement])
 
-// for (let i = 0; i <= 10; i++) {
-//   console.log("Number" + i);
-// }  //Output from Number 1 to Number 10
+for (let i = 0; i <= 10; i++) {
+  console.log("Number" + i);
+} //Output from Number 1 to Number 10
 
-// for (let j = 0; j <= 10; j = j + 2) {
-//   console.log("Number" + j);
-// } // Output Alternate Numbers
+for (let j = 0; j <= 10; j = j + 2) {
+  console.log("Number" + j);
+} // Output Alternate Numbers
 
 //**Nested Loop
 
-// for (let m = 0; m <= 10; m++) {
-//   console.log(m);
-//   {
-//     for (let n = 0; n <= 10; n++) {
-//       console.log(`${m}*${n}=${m * n}`);
-//     }
-//   }
-// }
+for (let m = 0; m <= 10; m++) {
+  console.log(m);
+  {
+    for (let n = 0; n <= 10; n++) {
+      console.log(`${m}*${n}=${m * n}`);
+    }
+  }
+}
+
+//
+//
 
 const names = ["John", "Jesse", "Sara", "Jim", "Tom"];
 
