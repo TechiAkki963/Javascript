e20e5580bd997af84648e351743a5241a2fe8c45
Akshay <paraba777@gmail.com>
2024-01-24 18:56:49 +0530

    DOM

---
+++ 07_DOM/59_DOM_/script.js
@@ -0,0 +1,18 @@
+title;
+
+title.textContent;
+
+const title = document.getElementsByClassName("heading");
+console.log(title);
+
+document.querySelector("h2");
+
+document.querySelector("#title");
+
+document.querySelector(".heading");
+/* <h1 id=​"title" class=​"heading">​…​</h1>​ */
+
+document.querySelector('input[type="password"]');
+/* writingMonitor.js:2 host: 127.0.0.1, id: , name: password, tagName: input, className: , isWebsiteDisabled: false, isWebsiteBlacklisted: false, appToolbarShown: false */
+
+document.querySelector("p:first-child");
